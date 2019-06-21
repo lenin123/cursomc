680b59ec1f56e00b13aecae6912323fd08059a3e
Lênin Müller <lenin_muller@uol.com.br>
2019-06-21 17:35:18 -0300

    Testando a primeira classe de domínio (Categorias)

--- src/main/java/com/leninmuller/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/leninmuller/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.leninmuller.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.leninmuller.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "rest está funcionando";
+	public List<Categoria> listar() {
+		Categoria cat = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listaCategorias = new ArrayList<>();
+		listaCategorias.add(cat);
+		listaCategorias.add(cat2);
+		
+		return listaCategorias;
 	}
 	
 
